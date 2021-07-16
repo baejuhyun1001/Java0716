a5a9427895cd2a0beb7d7d3775fa0ff2c257791b
testb <testb@DESKTOP-RAIKAOM>
2021-07-16 10:38:15 +0900

    PizzaExam

---
+++ PizzaExam/src/Pizza.java
@@ -0,0 +1,17 @@
+
+public class Pizza extends Circle{
+	private String name;
+	
+	
+	
+	public Pizza(int size, String name) {
+		super(size);
+		this.name = name;
+	}
+
+
+	@Override
+	public String toString() {
+		return "피자의 종류:" + name + ", 피자의 크기:"+size;
+	}
+}
